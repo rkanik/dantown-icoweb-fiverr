eb8c5412b9c1b3263d2e4ccc404ef415088d3c91
RK Anik <raselk@xpeedstudio.com>
2020-09-29 00:56:16 +0600

    Initialized

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,27 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div id="app">
+		<nav class="bg-secondary p-3">
+			<router-link class="mx-2 text-white" to="/">Home</router-link>
+			<router-link class="mx-2 text-white" to="/modals">Modals</router-link>
+			<router-link class="mx-2 text-white" to="/profile">Profile</router-link>
+		</nav>
+		<router-view class="p-3"></router-view>
+	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+	name: 'App',
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+	#app {
+		font-family: Avenir, Helvetica, Arial, sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		text-align: center;
+		color: #2c3e50;
+	}
 </style>
